196968aeeff33f07aeba6de313ede33057843e36
alafgani1000 <irhabyaganal625@gmail.com>
2020-05-20 16:22:51 +0700

    create form login

---
+++ components/Login.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import { StyleSheet } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Text, Body, Item, Input, Button } from "native-base";
+import axios from 'axios';
+
+export default class Login extends Component {
+  render() {
+      return (           
+          <Container style={styles.centerContent} >
+            <Content padder>
+              <Card>
+                <CardItem header bordered>
+                  <Text>NativeBase</Text>
+                </CardItem>
+                <CardItem bordered>
+                  <Body>
+                      <Item>
+                        <Input placeholder="Username" />
+                      </Item>
+                      <Item>
+                        <Input placeholder="Password" />
+                      </Item>
+                      <Button full info style={styles.loginButton}>
+                          <Text>Login</Text>
+                      </Button>
+                  </Body>
+                </CardItem>
+                <CardItem footer bordered>
+                  <Text>GeekyAnts</Text>
+                </CardItem>
+              </Card>
+            </Content>
+          </Container>  
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  centerContent : {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1
+  },
+  loginButton : {
+    marginTop: 10
+  }
+});
